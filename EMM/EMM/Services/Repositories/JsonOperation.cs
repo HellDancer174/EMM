4209da5bd29e9a594668deabad98d92d7dda6168
HellDancer174 <viktorignatev873@gmail.com>
2021-02-11 17:58:59 +0500

    Создал RoutesApi и JsonStringsApi + JsonOperation

---
+++ EMM/EMM/Services/Repositories/JsonOperation.cs
@@ -0,0 +1,42 @@
+﻿using ImmutableObject;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EMM.Services.Repositories
+{
+    public class JsonOperation : IJsonOperation
+    {
+        private readonly string jsonEntity;
+        private readonly HttpMethod method;
+        private readonly string accessToken;
+        private readonly HttpClient client;
+        private readonly string apiUrl;
+        private HttpRequestMessage request;
+
+        public JsonOperation(string jsonEntity, HttpMethod method, string accessToken, HttpClient client, string apiUrl)
+        {
+            this.jsonEntity = jsonEntity;
+            this.method = method;
+            this.accessToken = accessToken;
+            this.client = client;
+            this.apiUrl = apiUrl;
+        }
+
+        public async Task<HttpResponseMessage> Response()
+        {
+            return await client.SendAsync(request);
+        }
+
+        public void Send()
+        {
+            client.DefaultRequestHeaders.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("Bearer", accessToken);
+            var content = new StringContent(jsonEntity);
+            content.Headers.ContentType = new System.Net.Http.Headers.MediaTypeHeaderValue("application/json");
+            request = new HttpRequestMessage(method, apiUrl);
+            request.Content = content;
+        }
+    }
+}
---
+++ EMM/EMM/Trip/Repositories/RoutesApi.cs
@@ -0,0 +1,43 @@
+﻿using EMM.Helpers;
+using EMM.Models;
+using EMM.Services.Repositories;
+using ImmutableObject;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EMM.Trip.Repositories
+{
+    public class RoutesApi : IApiRepository<Route>
+    {
+        private readonly IJsonRepository jsonRepository;
+        private Action<HttpResponseMessage> validResponse;
+
+        public RoutesApi(IJsonRepository jsonRepository)
+        {
+            this.jsonRepository = jsonRepository;
+            validResponse = (response) => { if (!response.IsSuccessStatusCode) throw new HttpResponseException(response); };
+        }
+        public RoutesApi(string accessToken, HttpClient client):this(new JsonStringsApi(accessToken, client, "http://www.emmvi.online/api/Routes/"))
+        {
+        }
+        public async Task Send(Route entity, HttpMethod method)
+        {
+            var operation = jsonRepository.JsonOperation(JsonConvert.SerializeObject(entity), method);
+            operation.Send();
+            var response = await operation.Response();
+            validResponse(response);
+        }
+
+        public async Task<IEnumerable<Route>> Entities()
+        {
+            string json = await jsonRepository.Entities();
+            var routes = JsonConvert.DeserializeObject<IEnumerable<Route>>(json);
+            return routes;
+        }
+
+    }
+}
